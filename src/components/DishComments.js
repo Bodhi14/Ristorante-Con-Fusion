b7116cc08650daaf12159cd7bfb60db9c7c24d64
Bodhisattwa Das <72554471+Bodhi14@users.noreply.github.com>
2022-12-07 23:47:01 +0530

    Update DishComments.js

--- src/components/DishComments.js
+++ src/components/DishComments.js
@@ -1,29 +1,39 @@
-import {Card, CardBody} from 'reactstrap';
-import { comments } from '../shared/dishcomments';
+import { Card, CardBody } from "reactstrap";
+import { DISHES as dishes } from "../shared/dishes";
 
 function DishComments(props) {
-  
-  const comment_list = comments.map((comment) => {
-    return(
-     <ul>
-       {comment}
-       </ul>
-    )
+  const first_dish = dishes[0];
+
+  const comment_list = first_dish.comments.map((comm) => {
+    const date = comm.date.substring(0, 10);
+    const timezone = comm.date.substring(12, 26);
+    return (
+      <ul>
+        <p>
+          <strong>- - START TO THE TESTIMONIAL OF {comm.author} - - </strong>
+        </p>
+        <p>{comm.comment}</p>
+        <p>-- {comm.author}</p>
+        <p>Date: {date}</p>
+        <p>TimeZone: {timezone}</p>
+        <p>
+          <strong>- - END TO THE TESTIMONIAL OF {comm.author} - - </strong>
+        </p>
+      </ul>
+    );
   });
-   return(
-     <div className="col-12 col-md-5 m-1">
-     <Card>
-       <CardBody>
-         <ul>
-           <h1>
-             Comments
-             </h1>
-         </ul>
-         {comment_list}
-         </CardBody>
-     </Card>
-     </div>
-   )
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <Card>
+        <CardBody>
+          <ul>
+            <h1>Comments</h1>
+          </ul>
+          {comment_list}
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default DishComments;
